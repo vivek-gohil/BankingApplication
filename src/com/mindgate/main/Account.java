134e334c3d8baec4ec837761e552db9ef43b86a1
Vivek Gohil <ghl_vivek@hotmail.com>
2025-03-29 18:00:34 +0530

    Add Withdraw method

--- src/com/mindgate/main/Account.java
+++ src/com/mindgate/main/Account.java
@@ -40,6 +40,15 @@ public class Account {
 		this.balance = balance;
 	}
 
+	public boolean withdraw(double amount) {
+		if (amount > 0 && amount < balance) {
+			balance -= amount;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "Account [accountNumber=" + accountNumber + ", name=" + name + ", balance=" + balance + "]";
