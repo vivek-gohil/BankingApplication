b298f765a9fb57d5ed912d443ced137413072cb1
Vivek Gohil <ghl_vivek@hotmail.com>
2025-03-29 17:45:19 +0530

    Create new Account.java

---
+++ src/com/mindgate/main/Account.java
@@ -0,0 +1,48 @@
+package com.mindgate.main;
+
+public class Account {
+	private int accountNumber;
+	private String name;
+	private double balance;
+
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Account(int accountNumber, String name, double balance) {
+		super();
+		this.accountNumber = accountNumber;
+		this.name = name;
+		this.balance = balance;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [accountNumber=" + accountNumber + ", name=" + name + ", balance=" + balance + "]";
+	}
+
+}
