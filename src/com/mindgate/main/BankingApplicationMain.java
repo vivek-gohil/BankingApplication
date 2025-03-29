af86946677957d65177e0be3bf6630aa2f32fbad
Vivek Gohil <ghl_vivek@hotmail.com>
2025-03-29 17:28:51 +0530

    Create main class

---
+++ src/com/mindgate/main/BankingApplicationMain.java
@@ -0,0 +1,8 @@
+package com.mindgate.main;
+
+public class BankingApplicationMain {
+	public static void main(String[] args) {
+		System.out.println("BankingApplication stated");
+		System.out.println("BankingApplication end");
+	}
+}
