12c431210b012b11da0eaa9fc2fc62956e9bbc90
Vivek Gohil <ghl_vivek@hotmail.com>
2025-03-29 18:04:42 +0530

    Add deposit methdod

--- src/com/mindgate/main/Account.java
+++ src/com/mindgate/main/Account.java
@@ -40,6 +40,24 @@ public class Account {
 		this.balance = balance;
 	}
 
+	public boolean deposit(double amount) {
+		if (amount > 0) {
+			balance += amount;
+			return true;
+		} else
+			return false;
+	}
+
+	public boolean withdraw(double amount) {
+		if (amount > 0 && amount < balance) {
+			balance -= amount;
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
 	@Override
 	public String toString() {
 		return "Account [accountNumber=" + accountNumber + ", name=" + name + ", balance=" + balance + "]";
